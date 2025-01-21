2c06400376f8684eb90a45cec110d15fde04fdaa
Kasvit <kasvit93@gmail.com>
2025-01-21 21:37:36 +0200

    update CI/CD

--- spec/lib/rails_rate_limit/klass_spec.rb
+++ spec/lib/rails_rate_limit/klass_spec.rb
@@ -180,15 +180,15 @@ RSpec.describe RailsRateLimit::Klass do
 
     it "handles rate limit exceeded with custom handler" do
       2.times { instance.test_method }
-      
+
       result = instance.test_method
-      
+
       expect(result).to be_nil
     end
 
     it "does not raise error after custom handler execution" do
       2.times { instance.test_method }
-      
+
       expect { instance.test_method }.not_to raise_error
     end
   end
